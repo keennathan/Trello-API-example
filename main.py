6f04a06c3e4d597ef2c94e166868297c086c376f
keennathan <keennathan@icloud.com>
2024-08-17 10:51:06 +1000

    Adding CLI Cs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 
 from init import db, ma, bcrypt, jwt
+from controllers.cli_controllers import db_commands
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +13,9 @@ def create_app():
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    app.register_blueprint(db_commands)
+
 
     return app
 
